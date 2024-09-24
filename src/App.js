8f41bee3069b985ce4031e7d96bcbbec0ed1263e
Carlos Andrés Montoya Tobón <andresmontoyat@gmail.com>
2024-09-24 17:31:39 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ export default function App() {
       <Skill />
       <Contact />
       <Footer />
+  <iframe src="https://flypass-mapbox.netlify.app" allow="geolocation 'self' https://andresmontoyat.co"  width="100%" height="400" />
     </div>
   )
 }
